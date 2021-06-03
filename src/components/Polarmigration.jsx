f9156ce1adb79a8c950dd3df7d9ff0edfad45fcc
Adam <albsouladam@gmail.com>
2021-06-03 23:01:00 +0200

    bug fixes

--- src/components/Polarmigration.jsx
+++ src/components/Polarmigration.jsx
@@ -4,7 +4,6 @@ import {Helmet} from "react-helmet";
 
 import image1 from "../images/PM.webp";
 import image2 from "../images/polarmenu.webp";
-import { isNonNullExpression } from "typescript";
 
 function Polarmigration() {
   return (
